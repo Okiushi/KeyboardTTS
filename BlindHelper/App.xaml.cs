6ded61be2c88b0ecdbc953f4f13d973b23bd74d2
KillianFvt <killian.fauvette@gmail.com>
2023-09-18 22:10:51 +0200

    Added UI app to project

---
+++ BlindHelper/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace BlindHelper
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ BlindHelper/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+
+namespace BlindHelper
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SizeSlider_OnValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            Console.WriteLine(e.NewValue);
+        }
+    }
+}
